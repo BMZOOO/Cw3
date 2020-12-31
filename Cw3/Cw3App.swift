315fe28b933cc87af03e9b63c9a62a1e7b816075
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2020-12-31 20:16:28 +0300

    Initial Commit

---
+++ Cw3/Cw3App.swift
@@ -0,0 +1,17 @@
+//
+//  Cw3App.swift
+//  Cw3
+//
+//  Created by Batool  Jawaheri  on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct Cw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
